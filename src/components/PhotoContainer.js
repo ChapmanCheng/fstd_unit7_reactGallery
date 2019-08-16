00b2426e51960d7c123630358b78917028e3cf02
chapmanCC <chapboy1989@gmail.com>
2019-08-17 05:01:08 +0800

    PhotoContainer will show NotFound if no photos present

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Photo from "./Photo";
+import NotFound from "./NotFound";
 
 const PhotoContainer = ({ photos }) => {
-	let listOfPhotos = photos.map(photo => <Photo photo={photo} key={photo.id} />);
+	let listOfPhotos
+	if (photos.length > 0) {
+		listOfPhotos = photos.map(photo => <Photo photo={photo} key={photo.id} />);
+	} else {
+		listOfPhotos = <NotFound />
+	}
 
 	return (
 		<div className="photo-container">
 			<h2>Results</h2>
-			<ul>{listOfPhotos}</ul>
+			<ul>{listOfPhotos}</ul> :
 		</div>
 	);
 };
